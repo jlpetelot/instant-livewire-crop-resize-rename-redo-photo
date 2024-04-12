06b560e287bc023b4a21aa36bb542328fdd091c8
Jean-Luc Petelot <jlpetelot@madinici.org>
2024-04-12 14:03:26 +0200

    Cropping and resizing the image of temporary images in the storage/app/livewire-tmp folder.

--- app/Livewire/UploadPhotoLivewire.php
+++ app/Livewire/UploadPhotoLivewire.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\On;
+use Spatie\Image\Image;
 
 class UploadPhotoLivewire extends Component
 {
@@ -22,9 +23,35 @@ class UploadPhotoLivewire extends Component
 
     public function handleCroppedImage($croppedBlob, $cropRegions)
     {
-        // dd($croppedBlob, $cropRegions);
+        // We check which value ratio the width is (which can also be done by height).
+        $ratio  = $cropRegions['width'] / 1000;
 
+        // We apply this ratio for the height and width.
+        // If this ratio is greater than 1, the image will need to be resized.
+        if ($ratio > 1 ) {
+            // Si ce ratio est supérieur à 1, on divise la larh-geur et la hauteur des régions recasdrées par le ratio
+            $width = $cropRegions['width'] / $ratio;
+            $height = $cropRegions['height'] / $ratio;
+        }
+        // Otherwise, we leave it as it is.
+        else
+        {
+            // We leave it as it is.
+            $width = $cropRegions['width'];
+            $height = $cropRegions['height'];
+        }
+
+        // We modify the image according to this ratio and resize the image.
+        Image::load($this->image->getRealPath())
+            // We retrieve all parameters from $croppedBlob to pass them to $cropRegions, which will crop the image
+            ->resize($width, $height)
+            ->save();
+
+        // We display the image in the browser window.
         $this->croppedBlob = $croppedBlob;
+
+        // We go back to the current page.
+        // return redirect()->route('photo');
     }
 
     public function updatedImage ()
